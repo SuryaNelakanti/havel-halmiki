8f8bedcd98806a21d1ab04c75bbf6c3a633c9d72
Suryateja Nelakanti <47491728+SuryaNelakanti@users.noreply.github.com>
2019-07-27 10:19:07 +0530

    Update Secondtry.java

--- havelhalmiki/src/havelhalmiki/Secondtry.java
+++ havelhalmiki/src/havelhalmiki/Secondtry.java
@@ -1,3 +1,24 @@
+/*
+This algorithm will return true if the answers are consistent (i.e. it's possible that everyone is telling the truth) and false if the answers are inconsistent (i.e. someone must be lying):
+
+Remove all 0's from the sequence (i.e. warmup1).
+
+If the sequence is now empty (no elements left), stop and return true.
+
+Sort the sequence in descending order (i.e. warmup2).
+
+Remove the first answer (which is also the largest answer, or tied for the largest) from the sequence and call it N. The sequence is now 1 shorter than it was after the previous step.
+
+If N is greater than the length of this new sequence (i.e. warmup3), stop and return false.
+
+Subtract 1 from each of the first N elements of the new sequence (i.e. warmup4).
+
+Continue from step 1 using the sequence from the previous step.
+
+Eventually you'll either return true in step 2, or false in step 5.
+*/
+
+
 package havelhalmiki;
 import java.util.*;
 public class Secondtry {
@@ -71,7 +92,7 @@ public class Secondtry {
 		wrmup3(wrm1,k);
 		}
 		else
-			System.out.println("Satisfies Havel Halmiki criteria");
+			System.out.println("Satisfies Havel Halmiki criteria. True.");
 	} 
 	public static void wrmup3(int[] crr,int z) {
 		int N=0,i=0;
@@ -83,7 +104,7 @@ public class Secondtry {
 		}
 		int p =crr.length;
 		if(N>p) {
-			System.out.println("\nDoesn't satify the Havel Halmiki criteria");
+			System.out.println("\nDoesn't satify the Havel Halmiki criteria. False.");
 			return;
 		}
 		else {
